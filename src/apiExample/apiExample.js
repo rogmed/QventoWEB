48f47530ea22a0f768dc6db61b6dec13632444d2
Rogelio Medina <rogeliomdn@gmail.com>
2022-11-05 15:23:58 +0100

    Agrega ejemplos de uso de API

---
+++ src/apiExample/apiExample.js
@@ -0,0 +1,18 @@
+﻿var request = new XMLHttpRequest();
+apiKey = 'mQZ2D7eEycJ5SjBrt6W3zk1HgE3e52WB';
+
+request.open('GET', 'https://api.giphy.com/v1/gifs/random?api_key=' + apiKey + '& tag=squirrel & rating=g');
+
+request.onload = function () {
+	var response = request.response;
+	var parsedData = JSON.parse(response);
+	var originalUrl = parsedData.data.images.original.url;
+	console.log(originalUrl);
+
+	//Create gif on page
+	var gif = document.createElement('img');
+	gif.setAttribute('src', originalUrl);
+	document.body.appendChild(gif);
+};
+
+request.send();
---
+++ src/apiExample/qventoApiExample.js
@@ -0,0 +1,29 @@
+﻿var request = new XMLHttpRequest();
+
+var url = 'https://localhost:44371/qvento/'
+var qventoId = '1';
+
+request.open('GET', url + qventoId);
+
+request.onload = function() {
+	var response = request.response;
+	var qvento = JSON.parse(response);
+
+	var qventoId = qvento.qventoId;
+	var title = qvento.title;
+	var description = qvento.description;
+
+	console.log("Id: " + qventoId)
+	console.log("Title: " + title)
+	console.log("Description: " + description)
+
+	//Show qvento on page
+	const newDiv = document.createElement('div');
+	const newContent = document.createTextNode(title);
+	newDiv.appendChild(newContent);
+
+	const currentDiv = document.getElementById("div1");
+	document.body.insertBefore(newDiv, currentDiv);
+};
+
+request.send();
