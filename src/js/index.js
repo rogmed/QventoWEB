e5031a0aea2dd56f8f54f634d7dbbffd56b2b38d
Rogelio Medina Medina <rogelio.medina@unit4.com>
2022-11-25 16:30:04 +0100

    Arreglado login

--- src/js/index.js
+++ src/js/index.js
@@ -1,66 +1,20 @@
-//const url = "https://qvento.azurewebsites.net/"
-
-//const form = document.getElementById('login');
-
-//form.addEventListener('submit', callbackFunction);
-
-//function callbackFunction(event) {
-//    event.preventDefault();
-//    const myFormData = new FormData(event.target);
-//    console.log(myFormData);
-
-//    const credentialsDto = {};
-//    myFormData.forEach((value, key) => (credentialsDto[key] = value));
-//    console.log(credentialsDto);
-
-//    getOk();
-
-//    getResponse(credentialsDto);
-//}
-
-//function getResponse(dto) {
-//    var request = new XMLHttpRequest();
-
-//    request.open('POST', url + 'api/user/login');
-//    request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
-
-//    var response = request.response;
-//    console.log("- LOGIN RESPONSE\n" + response);
-
-//    request.send(JSON.stringify(dto))
-//}
-
-//function getOk() {
-//    var okRequest = new XMLHttpRequest();
-//    okRequest.addEventListener("load", callbackFunction);
-//    okRequest.open('GET', url + 'api/test');
-//    okRequest.send()
-
-//    var response = okRequest.response;
-//    console.log("- OK RESPONSE\n" + response);
-//}
-
 const request = new XMLHttpRequest();
-request.open('POST', 'https://qvento.azurewebsites.net/api/user/login');
 const form = document.getElementById('login')
 form.addEventListener('submit', callbackFunction);
 
-function callbackFunction(event) {
+function callbackFunction(e) {
 	event.preventDefault();
 
+	request.open('POST', 'https://qvento.azurewebsites.net/api/user/login');
 	request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
-	request.send(JSON.stringify(formJson(event)));
+	request.send(JSON.stringify(formJson(e)));
 	console.log(request)
 
-	while (true) {
-		if (request.readyState == 1) {
-			console.log("Status: " + request.status);
-			console.log("Response: " + request.response);
-			console.log("ResponseText: " + request.responseText);
-			break;
+	request.onreadystatechange = function() {
+		if (request.readyState == 4) {
+			window.location.href = request.response;
 		}
 	}
-
 };
 
 function formJson(event) {
