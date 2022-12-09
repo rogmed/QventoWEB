98134cd77739a8772ceafcc23dc7e5347ada12dd
Rogelio Medina <rogeliomdn@gmail.com>
2022-12-09 02:49:27 +0100

    Mensajes flotantes con Modals de Bootstrap

--- src/js/index.js
+++ src/js/index.js
@@ -7,9 +7,16 @@ const form = document.getElementById('login')
 // Listener para actuar cuando se haga click en 'submit'
 form.addEventListener('submit', callbackFunction);
 
+// Modal de bootstrap (alerta)
+const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
+
 
 // Funcion principal
 function callbackFunction(e) {
+	// Muestra mensaje mientras espera respuesta
+	$("#loginModal .modal-body").text('Esperando respuesta...');
+	loginModal.show();
+
 	// Esto evita que la consola dek navegador se limpie inmediatamente
 	event.preventDefault();
 
@@ -21,6 +28,15 @@ function callbackFunction(e) {
 	// Cuando la peticion cambie de estado se comprueba si está en 4 (DONE)
 	// y si ha recibido un 200 (OK) del servidor.
 	request.onreadystatechange = function () {
+
+		if (request.readyState == 4 && request.status == 401) {
+			$("#loginModal .modal-body").text('E-mail y/o password incorrectos.');
+		}
+
+		if (request.readyState == 4 && request.status == 400) {
+			$("#loginModal .modal-body").text('Se necesita e-mail y password.');
+		}
+
 		if (request.readyState == 4 && request.status == 200) {
 			// Abre la página que el servidor indica
 			window.location.href = request.response;
@@ -35,4 +51,9 @@ function formJson(event) {
 	myFormData.forEach((value, key) => (credentialsDto[key] = value));
 
 	return credentialsDto;
+}
+
+// Close the modal
+function closeDialog() {
+	loginModal.hide();
 }
