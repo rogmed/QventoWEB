9519e86c46c4ecdb9da5e3830ef03173623e2cb8
Rogelio Medina <rogeliomdn@gmail.com>
2022-12-15 00:19:42 +0100

    More

--- src/js/RegistrarUsuario.js
+++ src/js/RegistrarUsuario.js
@@ -1,29 +1,5 @@
 var email;
-
-const registrarUsuario = () => {
-    var name = document.getElementById("name").value;
-    var lastName = document.getElementById("lastname").value;
-    email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-    var confirmPassword = document.getElementById("confirm-password").value;
-    var phone = document.getElementById("phone").value;
-    var address = document.getElementById("address").value;
-
-    if (name === '' || lastName === '' || email === '' || password === '') {
-        alert("Los siguientes campos son obligatorios:"
-            + "\n - Nombre"
-            + "\n - Apellidos"
-            + "\n - E-mail"
-            + "\n - Contraseña");
-    } else if (password != confirmPassword) {
-        alert("La confirmación de la contraseña debe ser igual a la contraseña.");
-    } else { 
-        alert("Registro de"
-            + "\nNombre: " + name
-            + "\nApellidos: " + lastName
-            + "\ne-mail: " + email);
-    }
-}
+var message;
 
 // Inicia nueva peticion
 const request = new XMLHttpRequest();
@@ -31,33 +7,35 @@ const request = new XMLHttpRequest();
 // Selecciona el <form> con id 'login' para extraer datos
 const form = document.getElementById('register');
 
-// Listener para actuar cuando se haga click en 'submit'
-form.addEventListener('submit', callbackFunction);
-
 // Modal de bootstrap (alerta)
 const modal = new bootstrap.Modal(document.getElementById('modal'));
 
+// Listener para actuar cuando se haga click en 'submit'
+form.addEventListener('submit', callbackFunction);
+
 // Funcion principal
 function callbackFunction(e) {
+	// Esto evita que la consola dek navegador se limpie inmediatamente
+	e.preventDefault();
+
 	// Muestra mensaje mientras espera respuesta
 	$("#modal .modal-body").text('Esperando respuesta...');
 	$('#modal').modal('show');
 
-	// Esto evita que la consola dek navegador se limpie inmediatamente
-	e.preventDefault();
-
 	// Abre la petici�n, elije formato JSON y env�a el JSON en forma de string
 	request.open('POST', 'https://qvento-api.azurewebsites.net/api/user');
-	request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
-	request.send(JSON.stringify(formJson(e)));
+	request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+	var dto = formJson(e);
+	var response = request.send(JSON.stringify(dto));
 
 	// Cuando la peticion cambie de estado se comprueba si est� en 4 (DONE)
 	// y si ha recibido un 200 (OK) del servidor.
 	request.onreadystatechange = function () {
 		$('#modal').modal('show');
 
-		if (request.readyState == 4 && request.status == 400) {
-			$("#modal .modal-body").text('Http 400');
+		if (request.status == 400) {
+			var result = JSON.parse(response);
+			$("#modal .modal-body").text(result);
 		}
 
 		if (request.status == 404) {
@@ -65,8 +43,8 @@ function callbackFunction(e) {
 		}
 
 		if (request.status == 409) {
-			$("#modal .modal-body").text('El e-mail ' + email + " ya está en"
-			+ " uso. Por favor, utilice otro distinto.");
+			$("#modal .modal-body").text(dto["email"] + " ya está en uso. "
+			+ "Por favor, utilice un e-mail distinto.");
 		}
 
 		if (request.readyState == 4 && request.status == 422) {
@@ -75,7 +53,7 @@ function callbackFunction(e) {
 
 		if (request.readyState == 4 && request.status == 200) {
 			$("#modal .modal-body").text("Usuario registrado con éxito con "
-			+ " email: " + email);
+				+ " email: " + dto["email"]);
 
 			$("modal .modal.footer").text('Click para volver');
 			//window.location.href = "web.html";
@@ -83,12 +61,49 @@ function callbackFunction(e) {
 	}
 };
 
+// Comprueba si el formulario tiene los campos obligatorios
+function checkForm() {
+	var name = document.getElementById("name").value;
+	var lastName = document.getElementById("last-name").value;
+	email = document.getElementById("email").value;
+	var password = document.getElementById("password").value;
+	var confirmPassword = document.getElementById("confirm-password").value;
+
+	var message = "";
+	if (name === '' || lastName === '' || email === '' || password === '') {
+		var message = "Por favor, rellene: ";
+		if (name === "") {
+			message += "\n - Nombre";
+		}
+		if (lastName === "") {
+			message += "\n - Apellidos";
+		}
+		if (email === "") {
+			message += "\n - E-mail";
+		}
+		if (password === "") {
+			message += "\n - Contraseña";
+		}
+		$("#modal .modal-body").text(message);
+		$('#modal').modal('show');
+
+	} else if (password != confirmPassword) {
+		$("#modal .modal-body").text(
+			"La confirmación de la contraseña debe ser igual a la contraseña.");
+		$('#modal').modal('show');
+	} else {
+		return true;
+	}
+
+	return false;
+}
+
 // Coge todos los campos de <form> y crea un JSON con ellos
 function formJson(event) {
 	const dto = {};
 	const myFormData = new FormData(event.target);
 	myFormData.forEach((value, key) => {
-		if(key != "confirmPassword"){
+		if (key != "confirmPassword") {
 			dto[key] = value;
 		}
 	});
