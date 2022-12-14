c9fbe4a71fe4d9059add573b79378fed06a73f78
Rogelio Medina <rogeliomdn@gmail.com>
2022-12-14 20:07:37 +0100

    Fixes js

--- src/js/RegistrarUsuario.js
+++ src/js/RegistrarUsuario.js
@@ -1,18 +1,24 @@
 const registrarUsuario = () => {
     var name = document.getElementById("name").value;
-    var lastName = document.getElementById("last-name").value;
+    var lastName = document.getElementById("lastname").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
     var confirmPassword = document.getElementById("confirm-password").value;
     var phone = document.getElementById("phone").value;
     var address = document.getElementById("address").value;
 
-    if (password != confirmPassword) {
+
+    if (name === '' || lastName === '' || email === '' || password === '') {
+        alert("Los siguientes campos son obligatorios:"
+            + "\n - Nombre"
+            + "\n - Apellidos"
+            + "\n - E-mail"
+            + "\n - Contraseña");
+    } else if (password != confirmPassword) {
         alert("La confirmación de la contraseña debe ser igual a la contraseña.");
-    } else if (name === '' || lastName === '' || email === '' || password === '' ) {
-        alert("No se puede dejar ningún campo vacío")
-    } else {
-        console.log("Nombre: " + name
+    } else { 
+        alert("Registro de"
+            + "\nNombre: " + name
             + "\nApellidos: " + lastName
             + "\ne-mail: " + email);
     }
