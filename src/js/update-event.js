b298973ec5f58cee4fe02435a6c3e180a31485dd
Rogelio Medina <rogeliomdn@gmail.com>
2022-12-20 04:27:12 +0100

    Desactivar boton si no hay cambios en formulario

--- src/js/update-event.js
+++ src/js/update-event.js
@@ -1,6 +1,10 @@
 import { Cookie } from './cookie.js';
 import { Navbar } from './navbar.js';
 
+// Variables
+let initialValues;
+let currentValues;
+
 // Obtiene datos de las cookies
 const cookie = new Cookie();
 const token = JSON.parse(cookie.readCookie("token"));
@@ -18,20 +22,48 @@ const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
 // Comienza peticion
 const request = new XMLHttpRequest();
 
-const formulario = document.getElementById('formulario');
+const form = document.getElementById('formulario');
 
-formulario.addEventListener('submit', function (e) {
+// Desactiva botón de Modificar si los campos no han variado
+const button = document.querySelector('#submit-button');
+button.disabled = true;
+
+form.addEventListener('change', async (e) => {
+    e.preventDefault();
+
+    currentValues = await formJson(form);
+    console.log(currentValues);
+
+    for (const [key, value] of Object.entries(currentValues)) {
+        let compare;
+        if (key == 'DateOfQvento') {
+            compare = value.slice(0, -3);
+        } else {
+            compare = value.trim();
+        }
+
+        if (compare != initialValues[key]) {
+            console.log(`${compare} != ${initialValues[key]}`);
+            button.disabled = false;
+            return;
+        } else {
+            console.log(`${compare} == ${initialValues[key]}`);
+            button.disabled = true;
+        }
+    }
+})
+
+// Acción botón
+form.addEventListener('submit', function (e) {
     e.preventDefault();
 
     // Muestra mensaje mientras espera respuesta
     $("#loginModal .modal-body").text('Esperando respuesta...');
     loginModal.show();
 
-    const datos = new FormData(formulario);
-
     request.open('PUT', 'https://qvento-api.azurewebsites.net/api/qventos/' + qventoId);
     request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
-    request.send(JSON.stringify(formJson(e)));
+    request.send(JSON.stringify(formJson(form)));
 
     // Cuando la peticion cambie de estado se comprueba si está en 4 (DONE)
     // y si ha recibido un 200 (OK) del servidor.
@@ -44,13 +76,14 @@ formulario.addEventListener('submit', function (e) {
         if (request.readyState == 4 && request.status == 200) {
             // Abre la página que el servidor indica
             $("#loginModal .modal-body").text('Evento modificado con éxito');
+            initialValues = currentValues;
         }
     }
 })
 
-function formJson(event) {
+function formJson(form) {
     const qventoDto = {};
-    const myFormData = new FormData(event.target);
+    const myFormData = new FormData(form);
     let dateOfQvento = "";
     myFormData.forEach((value, key) => {
         if (key == 'DateOfQvento') {
@@ -63,23 +96,22 @@ function formJson(event) {
         }
 
         if (key != 'Date' && key != 'Time') {
-            qventoDto[key] = value
+            qventoDto[key] = value.trim();
         }
     });
     return qventoDto;
 }
 
 // Hace GET a la API y rellena los campos
-const listEventos = async () => {
+const getQvento = async () => {
     const response = await fetch("https://qvento-api.azurewebsites.net/api/qventos/" + qventoId);
-    const qvento = await response.json();
+    initialValues = await response.json();
 
     const { elements } = document.querySelector('form');
 
-    for (const [key, value] of Object.entries(qvento)) {
+    for (const [key, value] of Object.entries(initialValues)) {
 
         const field = elements.namedItem(key)
-        console.log(key + ": " + value);
         if (key == 'DateOfQvento') {
             let date = value.split("T")[0];
             field && (field.value = date);
@@ -93,5 +125,5 @@ const listEventos = async () => {
 };
 
 window.addEventListener("load", function () {
-    listEventos();
+    getQvento();
 });
