1f943fecfa2251ca0b6ade3e647a976bc9cea36b
Rogelio Medina <73010972+rogmed@users.noreply.github.com>
2023-01-16 00:50:45 +0100

    Create event modal (#34)
    * Add modal and clean update event
    
    * Delete second table in main page

--- src/js/create-event.js
+++ src/js/create-event.js
@@ -9,69 +9,24 @@ const token = JSON.parse(cookie.readCookie("token"));
 const navbar = new Navbar();
 navbar.load(token);
 
+// Modal de bootstrap (alerta)
+const modal = new bootstrap.Modal(document.getElementById('Modal'));
+
 const request = new XMLHttpRequest();
 
-const formulario = document.getElementById('formulario');
+const form = document.getElementById('formulario');
 
-const inputs = document.querySelectorAll('#formulario input');
+// Listener para actuar cuando se haga click en 'submit'
+form.addEventListener('submit', callbackFunction);
 
-const expresiones = {
-    title: /^[a-zA-Z0-9À-ÿ\s]{3,50}$/, // Alfanumérico (3 a 50 caracteres)
-    description: /^.{0,255}$/,
-    location: /^.{0,255}$/
-}
-
-const campos = {
-    createdBy: false,
-    title: false,
-    dateOfQvento: false,
-    status: false,
-    description: false,
-    location: false
-}
-
-const validarFormulario = (e) => {
-    switch (e.target.name) {
-        case "title":
-            validarCampo(expresiones.title, e.target, 'title');
-            break;
-        case "dateOfQvento":
-            validarCampo(expresiones.dateOfQvento, e.target, 'dateOfQvento');
-            break;
-        case "description":
-            validarCampo(expresiones.description, e.target, 'description');
-            break;
-        case "location":
-            validarCampo(expresiones.location, e.target, 'location');
-            break;
+function callbackFunction(e) {
+    if (checkForm(e)) {
+        sendRequest(e);
     }
 }
 
-const validarCampo = (expresion, input, campo) => {
-    if (expresion.test(input.value)) {
-        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
-        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
-        document.querySelector(`#grupo__${campo} i`).classList.add('fa-check-circle');
-        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-times-circle');
-        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
-        campos[campo] = true;
-    } else {
-        document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
-        document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
-        document.querySelector(`#grupo__${campo} i`).classList.add('fa-times-circle');
-        document.querySelector(`#grupo__${campo} i`).classList.remove('fa-check-circle');
-        document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
-        campos[campo] = false;
-    }
-}
-
-
-inputs.forEach((input) => {
-    input.addEventListener('keyup', validarFormulario);
-    input.addEventListener('blur', validarFormulario);
-});
-
-formulario.addEventListener('submit', function (e) {
+// Acción botón
+function sendRequest(e) {
     e.preventDefault();
 
     const datos = new FormData(formulario);
@@ -84,15 +39,16 @@ formulario.addEventListener('submit', function (e) {
 
     request.onreadystatechange = function () {
         if (request.readyState == 4 && request.status == 400) {
-            alert("Error. Faltan campos obligatorios.")
+            $("#Modal .modal-body").text("Error. Faltan campos obligatorios.");
         }
 
         if (request.readyState == 4 && request.status == 200) {
-            alert("Evento " + title + " creado!")
+            $("#Modal .modal-body").text("Evento " + title + " creado!");
             document.getElementById("formulario").reset();
         }
+        modal.show();
     }
-})
+}
 
 function formJson(event) {
     const qventoDto = {};
@@ -113,4 +69,38 @@ function formJson(event) {
         }
     });
     return qventoDto;
+}
+
+// Comprueba si el formulario tiene los campos obligatorios y construye el
+// mensaje relevante para devolverlo en un modal
+function checkForm(e) {
+    e.preventDefault();
+    const title = document.getElementById("title").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
+
+    let message;
+
+    if (title === '' || date === '' || time === '') {
+        message = "Por favor, rellene: ";
+
+        if (title === '') {
+            message += "\n - Título del evento";
+        }
+
+        if (date === '') {
+            message += "\n - Fecha";
+        }
+
+        if (time === '') {
+            message += "\n - Hora";
+        }
+
+        $("#Modal .modal-body").text(message);
+        $('#Modal').modal('show');
+        return false;
+
+    } else {
+        return true;
+    }
 }
